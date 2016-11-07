b3988e944e1a2213fe073f9aa334135e72cb623b
EC2 Default User <ec2-user@ip-172-31-32-133.us-west-2.compute.internal>
2016-11-07 00:04:34 +0000

    Lens works.
    Stole the company example from the tutorial .

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
 import           Control.Applicative
+import           Control.Lens.TH
+import           Control.Monad.State.Class (gets)
+import           Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Char8 as B
+import           Data.IORef
+import           Snap.Snaplet
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
+
+data App = App {
+    _companyName :: IORef B.ByteString
+}
+
+-- makeLenses ''App
 
 main :: IO ()
 main = quickHttpServe site
@@ -22,3 +36,16 @@ echoHandler = do
     param <- getParam "echoparam"
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
+
+companyHandler :: Handler App App ()
+companyHandler = method GET getter <|> method POST setter
+  where
+    getter = do
+        nameRef <- gets _companyName
+        name <- liftIO $ readIORef nameRef
+        writeBS name
+    setter = do
+        mname <- getParam "name"
+        nameRef <- gets _companyName
+        liftIO $ maybe (return ()) (writeIORef nameRef) mname
+        getter
